2717d8d22b668af3bfd6de9a3427f5c25628b87b
Andre Jarboe <andrejarboe@gmail.com>
2023-01-06 22:04:49 -0500

    filter all items dynamically

--- src/Categories.js
+++ src/Categories.js
@@ -1,20 +1,22 @@
 import React from 'react';
 
-const Categories = ({ filterItems}) => {
-    return (
-        //the manual way:
-			<div className="btn-container">
-				<button className="filter-btn" onClick={() => filterItems('all')}>
-					All
-				</button>
-				<button className="filter-btn" onClick={() => filterItems('breakfast')}>
-					Breakfast
-				</button>
-				<button className="filter-btn" onClick={() => filterItems('lunch')}>
-					Lunch
-				</button>
-			</div>
-		);
+const Categories = ({ categories, filterItems }) => {
+	return (
+		<div className="btn-container">
+			{categories.map((category, index) => {
+				return (
+					<button
+						key={index}
+						type="button"
+						className="filter-btn"
+						onClick={() => filterItems(category)}
+					>
+						{category}
+					</button>
+				);
+			})}
+		</div>
+	);
 };
 
 export default Categories;
